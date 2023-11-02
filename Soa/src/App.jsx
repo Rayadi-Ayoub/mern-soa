61c974bf3ef0dee5b7c059442eabcf8ecedd82a2
Rayadi-Ayoub <rayadiayoub7@gmail.com>
2023-11-03 00:58:57 +0100

    create header component

--- Soa/src/App.jsx
+++ Soa/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/profile";
+import About from "./pages/About";
+import Header from "./components/header";
 
 function App() {
-  return <div className="text-red-500 ">Rayadi Ayoub</div>;
+  return (
+    <BrowserRouter>
+      {/* header */}
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
