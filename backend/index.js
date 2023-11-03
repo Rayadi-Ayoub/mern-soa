230a7b89f432f9d14a1ad1a32d8d33138e7acea4
Rayadi-Ayoub <rayadiayoub7@gmail.com>
2023-11-03 02:29:08 +0100

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 import Express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = Express();
 app.listen(3000, () => {
