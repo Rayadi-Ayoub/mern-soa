66c02ccdc4e8ceb5cef7ab3963197c1f4d5eb502
Rayadi-Ayoub <rayadiayoub7@gmail.com>
2023-12-10 04:37:37 +0100

    private the profile page and update the headre

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/profile";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -13,10 +14,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<PrivateRoute/>}>
         <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -18,7 +18,7 @@ export default function OAuth() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          naame: result.user.displayName,
+          name: result.user.displayName,
           email: result.user.email,
           photo: result.user.photoURL,
         }),
