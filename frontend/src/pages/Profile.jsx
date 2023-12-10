faba79067787b572ddf97c875ad3afe3f5999ee3
Rayadi-Ayoub <rayadiayoub7@gmail.com>
2023-12-10 06:34:04 +0100

    signout function

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
