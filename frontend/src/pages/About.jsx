83bd4ecfe9c78d210e26f6394519bf6425008ed8
Rayadi-Ayoub <rayadiayoub7@gmail.com>
2023-12-11 01:51:10 +0100

    cleaning

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,28 +3,23 @@ import React from "react";
 export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About Sahand Estate
-      </h1>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About</h1>
       <p className="mb-4 text-slate-700">
-        Sahand Estate is a leading real estate agency that specializes in
-        helping clients buy, sell, and rent properties in the most desirable
-        neighborhoods. Our team of experienced agents is dedicated to providing
-        exceptional service and making the buying and selling process as smooth
-        as possible.
+        Chaibi , a premier real estate agency, specializes in assisting students
+        with renting properties in prime locations. Our dedicated team of agents
+        is committed to ensuring a seamless process for finding the ideal
+        student accommodation.
       </p>
       <p className="mb-4 text-slate-700">
-        Our mission is to help our clients achieve their real estate goals by
-        providing expert advice, personalized service, and a deep understanding
-        of the local market. Whether you are looking to buy, sell, or rent a
-        property, we are here to help you every step of the way.
+        Our mission is to support students in achieving their housing goals
+        through personalized service and a profound understanding of the local
+        rental market. Whether you're seeking a rental property,
       </p>
       <p className="mb-4 text-slate-700">
-        Our team of agents has a wealth of experience and knowledge in the real
-        estate industry, and we are committed to providing the highest level of
-        service to our clients. We believe that buying or selling a property
-        should be an exciting and rewarding experience, and we are dedicated to
-        making that a reality for each and every one of our clients.
+        our experienced agents are here to guide you at every step. With a
+        wealth of expertise in student housing, we strive to make your rental
+        experience exciting and rewarding, turning your housing needs into a
+        reality.
       </p>
     </div>
   );
